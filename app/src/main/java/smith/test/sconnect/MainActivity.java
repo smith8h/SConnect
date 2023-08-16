f017a671100b3bf2fc14db9258ce2eff43671076
Dev. Smith <husseinshakir81@gmail.com>
2023-08-17 02:19:58 +0300

    style(Sample Code): Update sample code.

--- app/src/main/java/smith/test/sconnect/MainActivity.java
+++ app/src/main/java/smith/test/sconnect/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import java.util.Map;
 import smith.lib.net.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SConnectCallBack {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,40 +16,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // simple example of how to use SConnect library.
+    // easy codes, and fast connections.
     public void check(View v) {
         
-        String tag = "someTag";
-        String url = "https://smithdev.t.me";
+        String tag = "someTag"; // optional.
+        String url = "https://smithdev.t.me"; // required.
         
-        if (SConnect.isDeviceConnected(this)) {
-            SConnect.with(this)
-                .callback(new SConnectCallBack() {
-                    @Override public void onSuccess(SResponse response, @NonNull String tag, Map<String, Object> responseHeaders) {
-                        if (tag.equals("someTag"))
-                            Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                        else
-                            Toast.makeText(MainActivity.this, "response of another request with another tag", Toast.LENGTH_SHORT).show();
-
-                        // a workaround of SResponse class
-                        SResponse.Array array = response.getArray();
-                        array.forEach(item -> {
-                            Object o = item;
-                        });
-
-                        SResponse.Map map = array.getMap(0);
-                        map.forEach((key, value) -> {
-                            String k = key;
-                            Object v = value;
-                        });
-                    }
-
-                    @Override public void onFailure(@NonNull SResponse response, String tag) {
-                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .url(url)
-                .tag(tag)
-                .get();
+        if (SConnect.isDeviceConnected(this)) { // check internet availability.
+            SConnect.with(this) // passing current context.
+                    .callback(this) // required to get responses.
+                    .url(url) // required.
+                    .tag(tag) // optional.
+                    .get(); // post, delete, push.
         }
+    }
+
+    @Override
+    public void onSuccess(SResponse response, @NonNull String tag, Map<String, Object> responseHeaders) {
+        if (tag.equals("someTag")) // check tag
+            Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show(); // get whole response as string
+        else
+            Toast.makeText(MainActivity.this, "response of another request with another tag", Toast.LENGTH_SHORT).show();
+
+        // a workaround of SResponse class
+        SResponse.Array array = response.getArray(); // get response if it was a json array
+        array.forEach(item -> { // iterate through its items
+            Object o = item;
+        });
+
+        SResponse.Map map = array.getMap(0); // get an object (map) from that array
+        map.forEach((key, value) -> { // iterate through its content by key and value for each data.
+            String k = key;
+            Object v = value;
+        });
+    }
+
+    @Override
+    public void onFailure(@NonNull SResponse response, String tag) {
+        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
     }
 }
