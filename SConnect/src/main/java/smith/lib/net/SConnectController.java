c3cff7892b7ca157444cad8d1b83d926a7cde937
Dev. Smith <husseinshakir81@gmail.com>
2023-09-25 00:04:54 +0300

    style(license): Add LICENSE & Copyright.

--- SConnect/src/main/java/smith/lib/net/SConnectController.java
+++ SConnect/src/main/java/smith/lib/net/SConnectController.java
@@ -1,15 +1,48 @@
+/*
+ * Copyright (C) 2023, Hussein Shakir (Dev. Smith)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package smith.lib.net;
 
 import android.annotation.SuppressLint;
+
 import androidx.annotation.NonNull;
+
 import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.security.SecureRandom;
-import java.security.cert.*;
-import java.util.*;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-import javax.net.ssl.*;
-import okhttp3.*;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @SuppressWarnings({"unused", "all"})
 class SConnectController {
